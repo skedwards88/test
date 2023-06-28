7569caf7f2c53744096b9cda96a821575a5119f0
Sarah Edwards <skedwards88@github.com>
2023-06-27 17:40:39 -0700

    Update myfile.rb

--- myfile.rb
+++ myfile.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# The Octocat class tells you about different breeds of Octocat
+# The Octocat class tells you about different breeds of Octocatr
 class Octocat
   def initialize(name, *breeds)
     # Instance variables
